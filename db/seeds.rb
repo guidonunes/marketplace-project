c019e3d232b14db9bda5909d26bd388af7a9236e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-06-05 15:49:58 -0300

    implement destroy method

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,19 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 require 'faker'
+
+puts "cleaning user..."
+User.destroy_all
+
 puts "creating user"
+
 user = User.create!(first_name: "Julian", last_name: "Rodelo", date_of_birth: Date.new(1990, 1, 1) , email: "juli@gmail.com", password: "123456")
 
 puts "user created"
+
+puts "cleaning products..."
+Product.destroy_all
+
 puts "creating products"
 10.times do
   product = Product.new(item_name: Faker::Music.band, user: user, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", price: rand(10000..200000))
